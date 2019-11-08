b9512b6052a285499c64e8d44118d6f76b76118d
alfonsoruiz <1800ruiz@gmail.com>
2019-11-07 19:11:56 -0800

    config

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const server = require('./server.js');
+require('dotenv').config();
 
-server.listen(8000, () => {
-  console.log('Server listening on port 8000');
+const server = require('./server.js');
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
 });
