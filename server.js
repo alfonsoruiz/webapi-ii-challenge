d52c5b905397ce7a8711897f34a096c3b5f1f3d3
alfonsoruiz <1800ruiz@gmail.com>
2019-11-07 19:17:59 -0800

    added rroot route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ server.use(express.json());
 
 const postsRouter = require('./postsRouter.js');
 
+server.get('/', (req, res) => {
+  res.send('Hello from Heroku');
+});
+
 server.use('/api/posts', postsRouter);
 
 module.exports = server;
